d137740a05229967aa831eb53f81edd3d2a04f9e
0x1 <kevin.francart3@gmail.com>
2026-07-14 01:33:01 +0200

    Remove unsafeOn (moved to b8x MiscFFI)

--- src/Node/HTTP/Server.php
+++ src/Node/HTTP/Server.php
@@ -2,10 +2,4 @@
 
 $exports = [];
 
-$exports['unsafeOn'] = function($emitter, $event, $listener) {
-    if (method_exists($emitter, 'on')) {
-        $emitter->on($event, $listener);
-    }
-};
-
 return $exports;
